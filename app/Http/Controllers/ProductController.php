ec5108dfac8743677ed27bc5b5c0b46e93acbdcc
syamar111-lab <syamar111@gmail.com>
2026-06-17 12:17:50 +0300

    ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,2 +1,49 @@
-// Product Controller 
-class ProductController { public function index(){ return "Products"; } } 
+public function frontPage()
+{
+    return "Front Page";
+}
+
+public function productList()
+{
+    return "Product List";
+}
+
+public function productDetail($id)
+{
+    return "Product Detail " . $id;
+}
+
+public function cart()
+{
+    return "Shopping Cart";
+}
+
+public function order()
+{
+    return "Order Page";
+}
+
+public function login()
+{
+    return "Login Page";
+}
+
+public function adminPanel()
+{
+    return "Admin Panel";
+}
+
+public function adminCategory()
+{
+    return "Admin Category";
+}
+
+public function adminProduct()
+{
+    return "Admin Product";
+}
+
+public function adminOrder()
+{
+    return "Admin Order";
+}
