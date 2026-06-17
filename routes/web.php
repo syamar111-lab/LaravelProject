06a0751299cd76c2ee940b2b8e89b609dd050ba8
syamar111-lab <syamar111@gmail.com>
2026-06-17 12:18:40 +0300

    web.php

--- routes/web.php
+++ routes/web.php
@@ -1,2 +1,12 @@
-// Routes file 
-Route::get('/products', 'ProductController@index'); 
+use App\Http\Controllers\ProductController;
+
+Route::get('/', [ProductController::class, 'frontPage']);
+Route::get('/products', [ProductController::class, 'productList']);
+Route::get('/product/{id}', [ProductController::class, 'productDetail']);
+Route::get('/cart', [ProductController::class, 'cart']);
+Route::get('/order', [ProductController::class, 'order']);
+Route::get('/login', [ProductController::class, 'login']);
+Route::get('/admin', [ProductController::class, 'adminPanel']);
+Route::get('/admin/category', [ProductController::class, 'adminCategory']);
+Route::get('/admin/product', [ProductController::class, 'adminProduct']);
+Route::get('/admin/order', [ProductController::class, 'adminOrder']);
